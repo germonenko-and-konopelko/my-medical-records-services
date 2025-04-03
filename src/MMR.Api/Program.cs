851e01ddb51bebf61e7aaadc724dcc683ab9a8c4
Slava Germonenko <slava.germonenko@gmail.com>
2025-04-03 20:00:42 +0200

    Set up JWT-based authorization.

--- src/MMR.Api/Program.cs
+++ src/MMR.Api/Program.cs
@@ -1,14 +1,55 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.WebHost.UseKestrel(kestrel => kestrel.AddServerHeader = false);
+
+builder.Services.Configure<JsonSerializerOptions>(jsonOptions =>
+{
+    jsonOptions.IncludeFields = true;
+    jsonOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+});
 
 builder.Services.AddOpenApi();
+builder.Services.AddLocalization();
+builder.Services.AddRequestLocalization(localization =>
+{
+    localization.SupportedUICultures = [
+        new CultureInfo("en"),
+        new CultureInfo("ru")
+    ];
+
+    localization.SetDefaultCulture("en");
+});
+
+builder.Services
+    .AddAuthentication()
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, jwtOptions =>
+    {
+        jwtOptions.MapInboundClaims = false;
+        jwtOptions.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+    });
+
+builder.Services
+    .AddAuthorizationBuilder()
+    .AddDefaultPolicy(
+        "User",
+        policy => policy.RequireClaim("user_id ").RequireClaim("email")
+    );
 
 var app = builder.Build();
 
 app.MapOpenApi();
 
-if (app.Environment.IsProduction())
+if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
